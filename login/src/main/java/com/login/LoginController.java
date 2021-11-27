9f8f916b3112a7dd98c406ab67e32982308b2236
Lorenzo Filipponi <pippolor3@gmail.com>
2021-11-27 15:53:37 +0100

    Update Login Window

---
+++ login/src/main/java/com/login/LoginController.java
@@ -0,0 +1,62 @@
+package com.login;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class LoginController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label accesLabel;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    void handleAccess(MouseEvent event) {
+
+    }
+
+    @FXML
+    void handleLogin(ActionEvent event) {
+        accesLabel.setText("Login Effettuato");
+    }   
+
+    @FXML
+    void handlePassword(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleUsername(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert accesLabel != null : "fx:id=\"accesLabel\" was not injected: check your FXML file 'Login.fxml'.";
+        assert loginButton != null : "fx:id=\"loginButton\" was not injected: check your FXML file 'Login.fxml'.";
+        assert password != null : "fx:id=\"password\" was not injected: check your FXML file 'Login.fxml'.";
+        assert username != null : "fx:id=\"username\" was not injected: check your FXML file 'Login.fxml'.";
+
+    }
+
+}
